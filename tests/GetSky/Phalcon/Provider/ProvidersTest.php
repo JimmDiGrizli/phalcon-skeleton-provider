7a44aae8a09ae3bec9494ddeb563ff7fdc7ffffd
JimmDiGrizli <gaa@szrcai.ru>
2014-02-18 14:50:40 +0400

    Update tests

--- tests/GetSky/Phalcon/Provider/ProvidersTest.php
+++ tests/GetSky/Phalcon/Provider/ProvidersTest.php
@@ -4,14 +4,24 @@ use GetSky\Phalcon\Provider\RouterProvider;
 use GetSky\Phalcon\Provider\SessionProvider;
 use GetSky\Phalcon\Provider\UrlProvider;
 use Phalcon\Config;
+use Phalcon\Mvc\Url;
+
 
 class ProvidersTest  extends PHPUnit_Framework_TestCase {
 
     public function testIsProvider()
     {
-        $router = new RouterProvider(new Config());
+        $router = new RouterProvider(new Config(
+            array(
+                "app"=>array("def_module"=>"frontend"),
+                "modules"=>array("frontend"=>"frontend")
+            )
+        )
+        );
         $session = new SessionProvider();
-        $url = new UrlProvider(new Config());
+        $url = new UrlProvider(
+            new Config(array("app"=>array("base_uri"=>"index")))
+        );
 
         $this->assertInstanceOf(
             'GetSky\Phalcon\AutoloadServices\Provider',
@@ -27,6 +37,19 @@ class ProvidersTest  extends PHPUnit_Framework_TestCase {
             'GetSky\Phalcon\AutoloadServices\Provider',
             $url
         );
+
+        $service = $url->getServices();
+        /**
+         * @var $url Url
+         */
+        $url = $service();
+        $this->assertSame("index", $url->getBaseUri());
+
+        $service = $session->getServices();
+        $this->assertInstanceOf('\Phalcon\Session\Adapter\Files', $service());
+
+        $service = $router->getServices();
+        $this->assertInstanceOf('\Phalcon\Mvc\Router', $service());
 
     }
 
